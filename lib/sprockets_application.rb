b32d60148dff815024074ab16edad04c33df4710
Thomas Bukowski <me@neodude.net>
2009-03-20 21:21:47 -0700

    removed redundent SprocketsOrganization module

--- lib/sprockets_application.rb
+++ lib/sprockets_application.rb
@@ -1,4 +1,15 @@
 module SprocketsApplication
-  extend SprocketsOrganization
   extend SprocketsMechanism
+  
+  def routes(map, method = :resource)
+    if method == :resource
+      map.resource(:sprockets, :only => :show)
+    elsif method == :resources
+      map.resources(:sprockets, :only => [:index, :show])
+    end
+  end
+  
+  def sprocket(name)
+    Sprocket.new(name)
+  end
 end
