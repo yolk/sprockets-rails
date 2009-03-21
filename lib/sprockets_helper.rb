51f2fa1fa30491bf4bd39346389458250ce3681a
Thomas Bukowski <me@neodude.net>
2009-03-20 20:52:24 -0700

    allow symbols to be passed into sprockets_include_tag

--- lib/sprockets_helper.rb
+++ lib/sprockets_helper.rb
@@ -1,7 +1,7 @@
 module SprocketsHelper
   def sprockets_include_tag(name = nil)
     if name
-      javascript_include_tag("/sprockets/#{ name }.js")
+      javascript_include_tag("/sprockets/#{ name.to_s }.js")
     else
       javascript_include_tag("/sprockets.js")
     end
